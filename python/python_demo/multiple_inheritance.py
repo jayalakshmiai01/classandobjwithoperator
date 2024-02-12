92cedbeb6b7ee46f8de0ba41548e32759b68852f
Jayalakshmi R <jayalakshmiai01@brightskills.com>
2024-02-12 13:52:20 +0530

    inheritance and pythonsql files added

---
+++ python/python_demo/multiple_inheritance.py
@@ -0,0 +1,39 @@
+class parent():
+    def __init__(self):
+        print("family name is bright services")
+        
+    def AadharAccess(self):
+        print("need AadharAccess")
+    def classRoom(self):
+        print("you can access calss room")
+    def lab1(self):
+        print("you can access lab1")
+    def lab2(self):
+        print("you can access lab2")
+    def aibatch(self):
+        print("you can  join  aibatch course")
+    def cadbatch(self):
+        print("you can join cadbatch course")
+class aibatch():
+    def c(self):
+        print("i study c")
+    def cpp(self):
+        print("i study cpp")
+    def python(self):
+        print("i study python")
+class cadbatch(parent,aibatch):
+    def __init__(self):
+        super().__init__()
+        super().AadharAccess()
+        super().classRoom()
+        super().lab1()
+        super().lab2()
+        super().aibatch()
+        super().cadbatch()
+        super().c()
+        super().cpp()
+        super().python()
+        
+
+cad=cadbatch()
+       
---
+++ python/python_demo/single_inheritance_ex_google.py
@@ -0,0 +1,12 @@
+class google():
+    def __init__(self):
+        print("welcome to google chrome")
+    def google_account(self):
+        print("you can login this account")
+    def search_bar(self):
+        print("you can search any webpages")
+class email(google):
+    pass
+e=email()
+e.google_account()
+e.search_bar()
